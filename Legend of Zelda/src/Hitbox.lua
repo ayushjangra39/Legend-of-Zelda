c38309ced74d42c7646c2f68a24aa4e0f35a6cb3
Ayush Jangra <135800490+ayushjangra39@users.noreply.github.com>
2025-06-10 14:19:27 +0530

    Add files via upload

---
+++ Legend of Zelda/src/Hitbox.lua
@@ -0,0 +1,13 @@
+--[[
+    Legend of Zelda
+
+]]
+
+Hitbox = Class{}
+
+function Hitbox:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+end
---
+++ Legend of Zelda/src/Projectile.lua
@@ -0,0 +1,18 @@
+--[[
+    Legend of Zelda
+
+]]
+
+Projectile = Class{}
+
+function Projectile:init()
+
+end
+
+function Projectile:update(dt)
+
+end
+
+function Projectile:render()
+
+end
